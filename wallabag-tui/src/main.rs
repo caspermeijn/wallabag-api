9be3f7f60f03afc90cbedb205d6cb08a3b88396e
Samuel Walladge <samuel@swalladge.id.au>
2018-11-27 14:34:41 +1030

    begin work on tui for wallabag based on tui-rs example

---
+++ wallabag-tui/src/main.rs
@@ -0,0 +1,157 @@
+mod event;
+
+use std::env;
+use std::fs::File;
+use std::io;
+
+use log::{debug, error, info, warn};
+use simplelog::WriteLogger;
+
+use termion::event::Key;
+use termion::input::MouseTerminal;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Corner, Direction, Layout, Rect};
+use tui::style::{Color, Modifier, Style};
+use tui::widgets::{Block, Borders, List, SelectableList, Text, Widget};
+use tui::Terminal;
+
+use wallabag_api::types::{AuthInfo, Config};
+use wallabag_api::Client;
+
+use crate::event::{Event, Events};
+
+struct App {
+    size: Rect,
+    client: Client,
+    selected: Option<usize>,
+    info_style: Style,
+    warning_style: Style,
+    error_style: Style,
+    critical_style: Style,
+}
+
+impl App {
+    fn new() -> App {
+        let config = Config {
+            auth_info: AuthInfo {
+                client_id: env::var("WALLABAG_CLIENT_ID").expect("WALLABAG_CLIENT_ID not set"),
+                client_secret: env::var("WALLABAG_CLIENT_SECRET")
+                    .expect("WALLABAG_CLIENT_SECRET not set"),
+                username: env::var("WALLABAG_USERNAME").expect("WALLABAG_USERNAME not set"),
+                password: env::var("WALLABAG_PASSWORD").expect("WALLABAG_PASSWORD not set"),
+            },
+            base_url: "https://framabag.org".to_owned(),
+        };
+
+        debug!("{:#?}", config);
+
+        let mut client = Client::new(config);
+        App {
+            size: Rect::default(),
+            client: client,
+            selected: None,
+            info_style: Style::default().fg(Color::White),
+            warning_style: Style::default().fg(Color::Yellow),
+            error_style: Style::default().fg(Color::Magenta),
+            critical_style: Style::default().fg(Color::Red),
+        }
+    }
+}
+
+fn main() -> Result<(), failure::Error> {
+
+    // init logging
+    WriteLogger::init(
+        simplelog::LevelFilter::Debug,
+        simplelog::Config::default(),
+        File::create("wallabag-tui.log").unwrap(),
+    );
+
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let events = Events::new();
+
+    // App
+    let mut app = App::new();
+
+    let items: Vec<String> = app
+        .client
+        .get_entries()
+        .unwrap()
+        .into_iter()
+        .map(|entry| entry.title.unwrap_or("untitled".to_owned()))
+        .collect();
+
+    loop {
+        let size = terminal.size()?;
+        if size != app.size {
+            terminal.resize(size)?;
+            app.size = size;
+        }
+
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                .split(app.size);
+
+            let style = Style::default().fg(Color::Black).bg(Color::White);
+            SelectableList::default()
+                .block(Block::default().borders(Borders::ALL).title("List"))
+                .items(&items)
+                .select(app.selected)
+                .style(style)
+                .highlight_style(style.fg(Color::LightGreen).modifier(Modifier::Bold))
+                .highlight_symbol(">")
+                .render(&mut f, chunks[0]);
+        })?;
+
+        match events.next()? {
+            Event::Input(input) => match input {
+                Key::Char('q') => {
+                    break;
+                }
+                Key::Left => {
+                    app.selected = None;
+                }
+                Key::Down => {
+                    app.selected = if let Some(selected) = app.selected {
+                        if selected >= items.len() - 1 {
+                            Some(0)
+                        } else {
+                            Some(selected + 1)
+                        }
+                    } else {
+                        Some(0)
+                    }
+                }
+                Key::Up => {
+                    app.selected = if let Some(selected) = app.selected {
+                        if selected > 0 {
+                            Some(selected - 1)
+                        } else {
+                            Some(items.len() - 1)
+                        }
+                    } else {
+                        Some(0)
+                    }
+                }
+                _ => {}
+            },
+            Event::Tick => {
+                debug!("tick");
+                // app.advance();
+            }
+        }
+    }
+
+    Ok(())
+}
