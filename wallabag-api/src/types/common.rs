41553a4e340c64304216f8b5066a38195b0d7e08
Samuel Walladge <samuel@swalladge.id.au>
2018-11-29 16:06:29 +1030

    impl deref for ID -> u32

--- wallabag-api/src/types/common.rs
+++ wallabag-api/src/types/common.rs
@@ -1,9 +1,10 @@
 use std::fmt;
+use std::ops::Deref;
 
 use serde_derive::{Deserialize, Serialize};
 
 /// The type used as an ID for all data structures. Declared for clarity.
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq)]
 pub struct ID(pub u32);
 
 impl fmt::Display for ID {
@@ -16,5 +17,32 @@ impl fmt::Display for ID {
 impl From<u32> for ID {
     fn from(x: u32) -> Self {
         ID(x)
+    }
+}
+
+
+/// For convenience. Automatic type coercion means that an `&ID` can be passed
+/// as an argument to a function that takes a `u32`. Hopefully will make it easier
+/// to work with `ID` values in the structs.
+impl Deref for ID {
+    type Target = u32;
+
+    fn deref(&self) -> &u32 {
+        &self.0
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn foo(arg: &u32) {}
+
+    #[test]
+    fn test_flexible_id() {
+        assert_eq!(*ID(234), 234);
+        assert_eq!(ID(234), 234.into());
+        foo(&ID(234));
     }
 }
